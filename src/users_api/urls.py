d6cf4fb33854537166eb791943fdf4c8d6395c9e
tunaDari <tunahan.darii@gmail.com>
2022-03-06 17:42:44 +0300

    Update API urls.

--- src/users_api/urls.py
+++ src/users_api/urls.py
@@ -5,7 +5,7 @@ from .views import UserList, UserDetail
 app_name = 'users'
 
 urlpatterns = [
-    path('user/<int:pk>/', UserDetail.as_view(), name='detailcreate'),
-    path('user/', UserList.as_view(), name='listcreate'),
+    path('<int:pk>/', UserDetail.as_view(), name='detailcreate'),
+    path('', UserList.as_view(), name='listcreate'),
 ] 
  
